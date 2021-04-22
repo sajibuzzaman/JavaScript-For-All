9a054cf816a7d79b19e2ebae3afcdbff6456a027
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-04-22 17:35:26 +0600

    learn variable

---
+++ chapter_two/variables.js
@@ -0,0 +1,7 @@
+var name = 'Sajib Uzzaman'
+var age = 21
+
+console.log(name + ' is a full stack web developer')
+console.log('His age is only ' + age)
+console.log(name + ' is creating many popular website')
+console.log('But his age is only ' + age)
