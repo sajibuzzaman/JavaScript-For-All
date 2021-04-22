9dd0de76e345d415e8ccdea583f79cf248af0e7e
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-04-22 17:19:01 +0600

    first commit

---
+++ chapter_two/hello.js
@@ -0,0 +1,11 @@
+console.log('Hello World!')
+console.log('I Love Bangladesh')
+
+console.log(80)
+console.log(75.88)
+
+// for together string and number output
+console.log('My favourite number is '+ 75)
+console.log('8'+ 8)
+
+console.log(8 + 8)
