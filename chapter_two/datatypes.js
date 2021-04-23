fb49250a5e239afed8cb8911461624e3213780ea
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-04-23 15:32:21 +0600

    data types & type conversion

---
+++ chapter_two/datatypes.js
@@ -0,0 +1,52 @@
+// Primitive Data
+
+// *** Numbers ***
+var n = 4586
+var m = 45.55
+
+var hex = 0xff      // hexadecimal pattern
+var oct = 0756      // octal pattern
+
+var x = '45'
+var y = Number('45.05')
+
+console.log(hex)
+console.log(oct)
+
+console.log(x)
+console.log(y)
+console.log(Number.parseFloat(x))
+console.log(Number.parseInt(y))
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(1 / 0)                      // return infinity
+console.log('abc' / 10)                 // return NaN(Not a Number) value
+
+
+// // *** String **
+
+// // literals
+// var str1 = 'Sajib Uzzaman'
+// var str2 = "Sajib Uzzaman"
+// var str3 = `Sajib Uzzaman`
+
+// // constructors
+// var str4 = String('Sajib Uzzaman')
+// var str5 = String(45)
+// var str6 = String(45.5)
+
+// console.log(str1, str2, str3, str4, str5, str6)
+
+// // *** Boolean ***
+
+// // literals
+// b1 = true
+// b2 = false
+
+// // constructors
+// b3 = Boolean(true)
+// b4 = Boolean(false)
+
+// console.log(b1, b2, b3, b4)
+
+// 
---
+++ chapter_two/typeconversion.js
@@ -0,0 +1,28 @@
+var str = '1000'
+var x = 10
+
+console.log(Number(str) + x)
+console.log(Number.parseInt(str))
+console.log(String(x))
+console.log(x.toString())
+console.log(Boolean(x))
+
+// // Falsy Values
+// ''
+// 0
+// null
+// undefined
+// NaN
+
+// console.log(Boolean(''))
+// console.log(Boolean('abc'))
+
+// console.log(Boolean(0))
+// console.log(Boolean(45))
+
+// console.log(Boolean(null))
+// console.log(Boolean(undefined))
+
+// console.log(true)
+// var x = true
+// console.log(x.toString())
