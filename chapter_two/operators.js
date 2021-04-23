6ab030e8b1f5c51f0874b54170936d61b93dca33
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-04-23 16:20:50 +0600

    operators

---
+++ chapter_two/operators.js
@@ -0,0 +1,64 @@
+// // Arithmetic Operators
+// // + - * / % ++ --
+
+// var a = 11
+// var b = 5
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+// console.log(a++)        // Post incremental
+// console.log(++a)        // Pre incremental
+// console.log(a--)        // Post decremental
+// console.log(--a)        // Pre decremental 
+
+
+// // Assignment operator
+// a = 30
+// b = 10
+
+// a += b
+// console.log(a)
+// a -= b
+// console.log(a)
+// a *= b
+// console.log(a)
+// a /= b
+// console.log(a)
+// a %= b
+// console.log(a)
+
+// // Comparison Operator
+// a = 20
+// b = 10
+// console.log(a == b)
+// console.log(a != b)
+// console.log(a < b)
+// console.log(a <= b)
+// console.log(a > b)
+// console.log(a >= b)
+
+// x = '50'
+// y = 50
+// console.log( x === y)
+// console.log( x !== y)
+
+// // Logical Operator
+// &&
+// ||
+// !
+
+// // Bitwise Operator
+// &
+// |
+// ~
+// ^
+// >>
+// <<
+
+// Type of operator
+a = 'abc'
+
+console.log(typeof a)
