20ca8b3386880904e911eaf436931936e1cbfd19
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-04-23 16:49:55 +0600

    date and math function

---
+++ chapter_two/mathfunction.js
@@ -0,0 +1,17 @@
+console.log(Math.E)
+console.log(Math.PI)
+
+n = -45.556
+console.log(Math.abs(n))
+n = 45.556
+console.log(Math.floor(n))
+console.log(Math.ceil(n))
+console.log(Math.round(n))
+
+console.log(Math.max(200, 50, 500))
+console.log(Math.min(200, 50, 500))
+console.log(Math.pow(2, 3))
+console.log(Math.sqrt(9))
+
+console.log(Math.random())
+console.log(Math.round(Math.random() * 50 + 1))
