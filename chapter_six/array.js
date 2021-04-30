1e373d379fe578b78ab1a6df0002c15d3f2cf6cd
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-04-30 22:16:37 +0600

    array

---
+++ chapter_six/array.js
@@ -0,0 +1,19 @@
+// declare array by literals
+var arr = [1, 2, 3, 4, 5]
+
+// insert data
+arr[5] = 6
+arr[10] = 10
+// update data
+arr[5] = 15
+
+console.log(arr)
+console.log(arr[1])
+console.log(arr[7])
+
+// length
+console.log(arr.length)
+
+// declare array by constructor
+var arr2 = Array(1,5,8)
+console.log(arr2) 
