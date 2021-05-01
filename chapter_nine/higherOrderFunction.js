92916d480ee74c6d4b52aaa502fdc87eecdb1112
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-05-01 23:47:39 +0600

    Higher Order Function

---
+++ chapter_nine/higherOrderFunction.js
@@ -0,0 +1,37 @@
+// 5. We can pass function as an arguments
+function add(a, b) {
+    return a+b
+}
+
+function manipulate(a, b, func) {
+    var c = a + b
+    var d = a - b
+
+    function multiply() {
+        var m = func(a, b)
+        return c*d*m
+    }
+    return multiply()
+}
+
+var multiply = manipulate(3, 2, add)
+console.log(multiply())
+
+// 6. We can return functions from another function
+
+function add(a, b) {
+    return a+b
+}
+
+function manipulate(a, b, func) {
+    var c = a + b
+    var d = a - b
+
+    return function() {
+        var m = func(a, b)
+        return c*d*m
+    }
+}
+
+var multiply = manipulate(3, 2, add)
+console.log(multiply())
