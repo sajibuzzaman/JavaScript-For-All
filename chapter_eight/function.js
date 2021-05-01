acee9d63ef2e1460dc538b34d7cec64db38180f7
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-05-01 21:41:08 +0600

    function define and call

---
+++ chapter_eight/function.js
@@ -0,0 +1,24 @@
+// function Define
+
+function functionName() {
+    console.log('I am a Function')
+}
+
+function add() {
+    var a = 10
+    var b = 20
+    console.log(a+b)
+}
+
+function sub() {
+    var a = 30
+    var b = 20
+    console.log(a-b)
+}
+
+// functionName()
+// add()
+// sub()
+for (var i=0; i<5; i++) {
+    functionName()
+}
