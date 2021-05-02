fecb92a350fda5f656fab25007b3356a34a35e7c
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-05-02 16:46:09 +0600

    forEach function

---
+++ chapter_nine/forEach.js
@@ -0,0 +1,19 @@
+var arr = [1, 2, 3, 4, 5]
+
+var sum = 0
+arr.forEach(function(value, index, array){
+    // console.log(value, index, array)
+    sum += value
+})
+console.log(sum)
+
+// **How its work in backend**
+function forEach(arr, cb) {
+    for(var i=0; i<arr.length; i++) {
+        cb(arr[i], i, arr)
+    }
+}
+
+forEach(arr, function(value, index, array) {
+    console.log(value, index, array)
+})
