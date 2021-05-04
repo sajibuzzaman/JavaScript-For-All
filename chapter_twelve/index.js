51a6c157c1f0f9a95c2c3869e6e2e8b79d60d027
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-05-04 16:08:29 +0600

    update and add this

--- chapter_twelve/index.js
+++ chapter_twelve/index.js
@@ -1,4 +1,4 @@
-// Literal Object
+// ***Literal Object***
 
 var rect = {
     // properties
@@ -8,6 +8,11 @@ var rect = {
     // method
     draw: function() {
         console.log('I am a Rectangle')
+        this.printPropertie()
+        // console.log(this)    //its return react object properties
+    },
+
+    printPropertie: function(){
         console.log('My Width is', this.width)
         console.log('My Height is', this.height)
     }
@@ -15,5 +20,22 @@ var rect = {
 
 rect.draw()
 
-rect.height = 50
-rect.draw()
+// rect.height = 50
+// rect.draw()
+
+// ***this**
+function func() {
+    console.log(this)       // its return windows object
+}
+func()
+
+var test = {
+    // properties
+    width: 20,
+    height: 10,
+    print: rect.printPropertie
+}
+test.print()
+
+
+// **factory pattern**
