cc29c7c5141ceb5ecb3db9c90187f7e2fdb9027d
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-05-05 21:44:35 +0600

    chapter thirteen and learn Inheritance

---
+++ chapter_thirteen/index.js
@@ -0,0 +1,53 @@
+// // ***Inheritance***
+
+// function person(name) {
+//     this.name = name
+// }
+//  var p1 = new person('Sajib Uzzaman')
+//  console.log(p1)
+
+
+// var person = {
+//     name: 'Sajib Uzzaman'
+// }
+
+// // console.log(person)
+
+// // for(var i in person) {
+// //     console.log(i)
+// // }
+
+// // console.log(Object.keys(person))
+
+// // var descriptor = Object.getOwnPropertyDescriptor(person, 'name')
+// // console.log(descriptor)
+
+// // let baseObject = Object.getPrototypeOf(person, 'name')
+// // let descriptor = Object.getOwnPropertyDescriptor(baseObject, 'toString')
+// // console.log(descriptor)
+
+// Object.defineProperty(person, 'name', {
+//     enumerable: false,
+//     writable: false,
+//     configurable: false,
+//     value: 'Sajib',
+// })
+
+// // ***Constructor Prototype**
+// function Person(name) {
+//     this.name = name
+// }
+
+// Person.prototype.PI = 3.12
+
+// var p1 = new Person('Sajib Uzzaman')
+// var p2 = new Person('Maisha')
+
+// // console.log(Object.getPrototypeOf(p1))
+// // console.log(Person.prototype)
+
+// console.log(p1)
+// console.log(p2)
+
+
+// ***Instance and Prototype Member***
