65b2f8bd1e4811621ba4bba8901b6d04f61c2645
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-05-26 18:45:23 +0600

    Chapter fifteen ES6

---
+++ chapter_fifteen/src/scripts/index.js
@@ -0,0 +1,10 @@
+import '../styles/index.scss'
+
+import { sum, sub  } from './math'
+
+console.log('Hello Sajib Uzzaman')
+
+console.log(sum(10, 20))
+console.log(sub(10, 20))
+
+alert('Hello Sajib')
