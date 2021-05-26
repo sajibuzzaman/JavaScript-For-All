29a22ac779978530c4cb0ad2cb6c54655861e444
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-05-26 16:43:18 +0600

    prototypical Inheritance

---
+++ chapter_fourteen/index.js
@@ -0,0 +1,33 @@
+function Shape(){
+
+}
+
+Shape.prototype = {
+    common: function(){
+        console.log('I am common function')
+    }
+}
+
+// inherite Shape function
+function Square(width) {
+    this.width = width
+}
+
+Square.prototype = Object.create(Shape.prototype)
+
+Square.prototype.draw = function() {
+    console.log('I am drawing')
+}
+
+var sqr1 = new Square(50)
+sqr1.common()
+
+// inherite Shape function
+function Circle() {
+    console.log('I am Circle')
+}
+
+Circle.prototype = Object.create(Shape.prototype)
+
+var circle1 = new Circle()
+circle1.common()
