7ededd8ac29a778ef14e60252224dfc35d5cb6a1
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-05-30 11:59:43 +0600

    var vs let vs const

--- chapter_fifteen/src/scripts/index.js
+++ chapter_fifteen/src/scripts/index.js
@@ -1,24 +1,64 @@
 import '../styles/index.scss'
 
-// //  |Template String
-// var s = `   dsdwd
-// awdad axax
-// awfafdaes
-//     sacasc AZXax
+// // //  |Template String
+// // var s = `   dsdwd
+// // awdad axax
+// // awfafdaes
+// //     sacasc AZXax
     
-//     asdxascdas`
+// //     asdxascdas`
 
-// console.log(s.trim())
+// // console.log(s.trim())
 
-var name = 'Sajib Uzzaman'
-var age = 22
+// var name = 'Sajib Uzzaman'
+// var age = 22
 
-console.log( 'My name is ' + name + ' and I am ' + age + ' years old')
-console.log(`My name is ${name} and I am ${age} years old. I am ${age > 18? '': 'not '}adult`)
+// console.log( 'My name is ' + name + ' and I am ' + age + ' years old')
+// console.log(`My name is ${name} and I am ${age} years old. I am ${age > 18? '': 'not '}adult`)
 
 
-// new method
-console.log(name.padStart(15))
-console.log(name.padStart(15, '*'))
-console.log(name.padEnd(15, 'a'))
-console.log('S'.repeat(10))
+// // new method
+// console.log(name.padStart(15))
+// console.log(name.padStart(15, '*'))
+// console.log(name.padEnd(15, 'a'))
+// console.log('S'.repeat(10))
+
+// ***let vs const vs var***
+
+// let a = 10
+// a = 100
+// console.log(a)
+
+// const b = 20
+// // b = 200
+// console.log(b)
+
+// if (true) {
+//     var a = 10
+//     let b = 20
+// }
+
+// console.log(a)
+// console.log(b)
+
+// for (var i = 1; i<5; i++) {
+
+// }
+// console.log(i)
+
+// for (let i = 1; i<5; i++) {
+
+// }
+// console.log(i)
+
+// {
+//     let iAmLet = 'I am let'
+//     console.log(iAmLet)
+// }
+// // console.log(iAmLet)
+
+// (function(){
+//     var abc = 10
+//     console.log(abc)
+// })()
+// // console.log(abc)
